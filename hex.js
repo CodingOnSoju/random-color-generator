d685062e1c9e26043c420c78cd3e57a57b0a104c
Angie <dreamcaster000@gmail.com>
2023-05-24 16:19:18 +0800

    Updated hex file to generate random hexadecimal colours

--- hex.js
+++ hex.js
@@ -1,1 +1,24 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+// #ff15025
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+// click events and call back functions
+btn.addEventListener("click", function () {
+  //create a hexColor
+  let hexColor = "#";
+  //loop it 6 times to generate hexadecimal color
+  for (let i = 0; i < 6; i++) {
+    //invoke the function
+    hexColor += hex[getRandomNumber()];
+  }
+  //The textContent property in JavaScript is used to
+  //get or set the text content of an HTML element.
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+// set up function to return a random number
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
